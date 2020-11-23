99302fc7b94de60694de5363e7cb47632b914ae3
getrajer533 <getrajer533@gmail.com>
2020-11-23 07:09:23 +0000

    Add project files.

---
+++ MyGoalPlanner/Models/DailyGoal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyGoalPlanner.Models
+{
+    public class DailyGoal
+    {
+        public int Id { get; set; }
+
+    }
+}
