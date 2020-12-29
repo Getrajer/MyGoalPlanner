648a79361551a78c78a6518aaebadd6bcf964030
Mateusz Owczarek <getrajer533@gmail.com>
2020-12-29 20:15:13 +0000

    Up

---
+++ MyGoalPlanner.Api/Controllers/GoalsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyGoalPlanner.Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyGoalPlanner.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GoalsController : ControllerBase
+    {
+        private readonly IGoalRepository goalRepository;
+
+        public GoalsController(IGoalRepository goalRepository)
+        {
+            this.goalRepository = goalRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetGoals()
+        {
+            try
+            {
+                return Ok(await goalRepository.GetAllGoals());
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Error geting data from the database");
+            }
+
+            
+        }
+
+    }
+}
