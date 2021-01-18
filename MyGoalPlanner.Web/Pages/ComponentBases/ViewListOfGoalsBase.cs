5f06f7a138de1bc731ca05a637ec5a3245716579
Mateusz Owczarek <getrajer533@gmail.com>
2021-01-18 07:43:44 +0000

    Adding Bases

---
+++ MyGoalPlanner.Web/Pages/ComponentBases/ViewListOfGoalsBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyGoalPlanner.Web.Pages.ComponentBases
+{
+    public class ViewListOfGoalsBase
+    {
+    }
+}
---
+++ MyGoalPlanner.Web/Pages/PartialComponents/PartialBases/AddGoalTypeBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyGoalPlanner.Web.Pages.PartialComponents.PartialBases
+{
+    public class AddGoalTypeBase
+    {
+    }
+}
