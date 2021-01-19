89340db1fe011b1c378d12f20b80411d8b6f2783
Mateusz Owczarek <getrajer533@gmail.com>
2021-01-19 22:31:37 +0000

    Some more f

--- MyGoalPlanner.Web/Pages/ComponentBases/ViewGoalBase.cs
+++ MyGoalPlanner.Web/Pages/ComponentBases/ViewGoalBase.cs
@@ -1,11 +1,30 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Components;
+using MyGoalPlanner.Models;
+using MyGoalPlanner.Web.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace MyGoalPlanner.Web.Pages.ComponentBases
 {
-    public class ViewGoalBase
+    public class ViewGoalBase : ComponentBase
     {
+        [Inject]
+        public IGoalService GoalService { get; set; }
+
+        [Parameter]
+        public int GoalId { get; set; }
+
+        protected Goal goal = new Goal();
+
+
+
+        protected override async Task OnInitializedAsync()
+        {
+            goal = await GoalService.GetGoal(19);
+        }
+
+
     }
 }
--- MyGoalPlanner.Web/Pages/ComponentBases/ViewListOfGoalsBase.cs
+++ MyGoalPlanner.Web/Pages/ComponentBases/ViewListOfGoalsBase.cs
@@ -1,11 +1,27 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Components;
+using MyGoalPlanner.Models;
+using MyGoalPlanner.Web.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace MyGoalPlanner.Web.Pages.ComponentBases
 {
-    public class ViewListOfGoalsBase
+    public class ViewListOfGoalsBase : ComponentBase
     {
+        [Inject]
+        public IGoalService GoalService { get; set; }
+
+        protected List<Goal> goals = new List<Goal>();
+
+
+        protected override async Task OnInitializedAsync()
+        {
+            var goalss = await GoalService.GetGoals();
+            goals = goalss.ToList();
+        }
+
+
     }
 }
