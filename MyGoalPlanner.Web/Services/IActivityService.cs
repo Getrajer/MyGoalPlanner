8bd130b7338e23e74d0df392b670edc1ff53e571
Mateusz Owczarek <getrajer533@gmail.com>
2021-02-11 22:51:08 +0000

    Activity Service

---
+++ MyGoalPlanner.Web/Services/IActivityService.cs
@@ -0,0 +1,15 @@
+﻿using MyGoalPlanner.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyGoalPlanner.Web.Services
+{
+    public interface IActivityService
+    {
+        Task<Activity> GetActivity(int activityId);
+        Task<Activity> CreateActivity(Activity newActivity);
+        Task DeleteActivity(int activityId);
+    }
+}
---
+++ MyGoalPlanner.Web/Services/ServiceActivity.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Components;
+using MyGoalPlanner.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace MyGoalPlanner.Web.Services
+{
+    public class ServiceActivity : IActivityService
+    {
+        private readonly HttpClient httpClient;
+
+        public ServiceActivity(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+    }
+
+        public async Task<Activity> CreateActivity(Activity newActivity)
+        {
+            return await httpClient.PostJsonAsync<Activity>("api/Activity", newActivity);
+        }
+
+        public async Task DeleteActivity(int activityId)
+        {
+            await httpClient.DeleteAsync($"api/Activity/{activityId}");
+        }
+
+        public async Task<Activity> GetActivity(int activityId)
+        {
+            return await httpClient.GetJsonAsync<Activity>($"api/Activity/{activityId}");
+        }
+    }
+}
