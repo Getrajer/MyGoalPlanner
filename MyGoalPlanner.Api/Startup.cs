e0e1f2c4873228a4a4916d62fea530eec547abc5
Mateusz Owczarek <getrajer533@gmail.com>
2021-03-20 19:51:35 +0000

    Up

--- MyGoalPlanner.Api/Startup.cs
+++ MyGoalPlanner.Api/Startup.cs
@@ -2,16 +2,16 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using MyGoalPlanner.Api.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MyGoalPlanner.Api.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace MyGoalPlanner.Api
 {
